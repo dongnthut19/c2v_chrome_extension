855e6ca1e5356544b3991da4f04d97f87814e5a5
dongnguyentat <dong.nguyen@mycleo.net>
2025-03-10 06:55:06 +0700

    fix

--- src/app/assets/js/const.js
+++ src/app/assets/js/const.js
@@ -1,4 +1,4 @@
-﻿var cartURL = "https://c2v.store/gio-hang";
-var apiURL= "https://c2v.store/api/";
+﻿var cartURL = "https://c2v.vn/gio-hang";
+var apiURL = "https://c2v.vn/api/";
 var ndt_vnd = 3700;
 var serviceFee = 10000;
